8f179c39417536a4f7d8650602df1dcee0b0e786
Edgar V <evenegas9@ucmerced.edu>
2019-09-20 09:14:07 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -6,6 +6,6 @@ class Patient
   end
 
   def new_appointment doctor, date
-    appointment = Appointment.new(date, patient, doctor)
+    appointment = Appointment.new(date, self, doctor)
   end
 end
