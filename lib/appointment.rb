e292e810c4da28560be75ebc26a2672dc4c8aff2
Edgar V <evenegas9@ucmerced.edu>
2019-09-20 09:05:34 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment
   attr_accessor :date, :patient, :doctor
+  @@all = []
+
+  def initialize date, patient, doctor
+    @date = date
   "initializes with a date, patient, and doctor"
 end
